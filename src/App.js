9c42605e60e36efbfbeddba1735de43e2d441eaa
Adrien <tchtadrien@gmail.com>
2023-11-21 22:10:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,20 +2,42 @@ import { useEffect, useState } from "react";
 
 import "./App.css";
 
-// CYCLE DE VIE du composant App :
-// 1. rendu initial (avec les valeurs d'état initiales)
-// 2. exécution de l'action du `useEffect` : mise à jour de l'état
-// 4. ce qui fait automatiquement un nouveau rendu
-
 function App() {
-  // déclarer l'état pour stocker les notes
   const [notes, setNotes] = useState(null);
+  const [selected, setSelected] = useState(0)
 
   async function fetchNotes() {
-    const response = await fetch("/notes");
+    const response = await fetch("http://localhost:4000/notes");
     const data = await response.json();
+    setNotes(data)
     console.log(data);
   }
+
+  async function addNote() {
+    try {
+      const response = await fetch("http://localhost:4000/notes", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: notes.length,
+          title: "Nouvelle note "+notes.length,
+          content: "Contenu de la nouvelle note "+notes.length,
+        }),
+      });
+
+      if (response.ok) {
+        // Actualiser la liste des notes après l'ajout
+        fetchNotes();
+      } else {
+        console.error("Erreur lors de l'ajout de la note");
+      }
+    } catch (error) {
+      console.error("Erreur lors de la requête :", error);
+    }
+  }
+
 
   useEffect(function () {
     fetchNotes();
@@ -24,9 +46,14 @@ function App() {
   return (
     <>
       <aside className="Side">
-        {notes !== null ? notes.map((note) => <div></div>) : null}
+        <button className="Button-create-note" onClick={()=>addNote()}>+</button>
+        {notes !== null ? notes.map((note) => (
+        <div className="Note-link" onClick={() => setSelected(note.id)}>{note.title}</div>))
+      : null}
       </aside>
-      <main className="Main"></main>
+      <main className="Main">
+        {notes !== null ?  <a>{notes[selected].content}</a> : <p>Chargement...</p>}
+      </main>
     </>
   );
 }
